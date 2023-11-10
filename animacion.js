95e6c2abe878fdf2be3fd01a57a9b39e345c186d
SISMP13 <124921373+SISMP13@users.noreply.github.com>
2023-11-10 11:22:23 +0100

    Add files via upload

---
+++ animacion.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal();
+    sr.reveal('h1', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    })
+
+    sr.reveal('a', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    })
